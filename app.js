2b9bcfee5b9da3cd26e0fc3bf7c65c9e38374ea2
Mahi-Rathod <s20_rathod_mahesh@mgmcen.ac.in>
2024-10-08 21:09:11 +0530

    created login form profile, username and password field

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ container.appendChild(navbar);
 
 const menuList = document.createElement("ul");
 navbar.appendChild(menuList);
-
 const home = document.createElement("li");
 home.innerText = "Home";
 
@@ -25,3 +24,64 @@ menuList.appendChild(home);
 menuList.appendChild(services);
 menuList.appendChild(product);
 menuList.appendChild(contactUs);
+
+//Login container code work
+
+const loginContainer = document.createElement("div");
+loginContainer.classList.add("login-container");
+
+const headLine = document.createElement("h3");
+headLine.innerText = "Login";
+headLine.style.color = "white";
+headLine.style.textAlign = "center";
+loginContainer.appendChild(headLine);
+
+const profile = document.createElement("div");
+profile.classList.add("profile")
+profile.style.backgroundImage = "url('https://ik.imagekit.io/d9mvewbju/Course/BigbinaryAcademy/profile-image-css-login-page-project_tS9W4AV3q.png')"
+loginContainer.appendChild(profile);
+
+const loginForm = document.createElement("form");
+loginContainer.appendChild(loginForm);
+
+container.appendChild(loginContainer);
+
+
+//Username 
+const userNameContainer = document.createElement("div");
+userNameContainer.classList.add("username-container");
+
+loginForm.appendChild(userNameContainer);
+
+const lableForUsername = document.createElement("label");
+
+lableForUsername.setAttribute("for", "username");
+
+lableForUsername.textContent = "Username";
+
+userNameContainer.appendChild(lableForUsername);
+
+const userName = document.createElement("input");
+userName.setAttribute("type", "text");
+userName.setAttribute("id", "username");
+
+userNameContainer.appendChild(userName);
+
+
+//password
+const passwordContainer = document.createElement("div");
+passwordContainer.classList.add("password-container");
+
+loginForm.appendChild(passwordContainer);
+
+const labelForPassword = document.createElement("label");
+labelForPassword.setAttribute("for", "password");
+
+labelForPassword.textContent = "Password";
+passwordContainer.appendChild(labelForPassword);
+
+const password = document.createElement("input");
+password.setAttribute("type", "password");
+password.setAttribute("id", "password");
+
+passwordContainer.appendChild(password);
